dc45eaf36654cf253db75de046428e225a451b87
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2024-03-02 23:41:26 -0600

    Update fileChanges.sh

--- Linux/General/monitorScripts/fileChanges.sh
+++ Linux/General/monitorScripts/fileChanges.sh
@@ -4,7 +4,7 @@ echo -e "FILE CHANGE TRACKER:\n"
 echo -e "---------------------\n"
 md5sum /etc/passwd /etc/group /etc/profile /etc/sudoers /etc/hosts /etc/ssh/ssh_config /etc/ssh/sshd_config > /ccdc/temp3
 ls -a /etc/ /usr/ /sys/ /home/ /bin/ /etc/ssh/ >> /ccdc/temp3
-fileChanges=$(diff temp2 temp3)
+fileChanges=$(diff /ccdc/temp2 /ccdc/temp3)
 if [[ ! -z "$fileChanges" ]];then
   	echo -e "CHANGE TRACKER:"
 	echo -e "\n"
