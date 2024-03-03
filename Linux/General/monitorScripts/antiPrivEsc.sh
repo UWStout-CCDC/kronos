4c6b7b8eb77c67a1e38d19ce6dcaf755a955bbb7
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2024-03-02 22:58:08 -0600

    Update antiPrivEsc.sh

--- Linux/General/monitorScripts/antiPrivEsc.sh
+++ Linux/General/monitorScripts/antiPrivEsc.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 # Finds all UID 0 accounts
-echo "UID 0 accounts:"
 getent passwd | grep '0:0' | cut -d':' -f1 > /ccdc/uid0.txt
-cat /ccdc/uid0.txt
 
 # Find all users w/ sudo privs
-echo "----------------------"
-echo "Users with sudo privs:"
 grep -E '^[^#%@]*\b(ALL|(S|s)udoers)\b' /etc/sudoers > /ccdc/sudoers.txt
-cat /ccdc/sudoers.txt
 
 # SUID binaries
+find / -uid 0 -perm -4000 -print 2>/dev/null > /ccdc/suid.txt
+
+echo "UID 0 accounts:"
+cat /ccdc/uid0.txt
+
+
+echo "----------------------"
+echo "Users with sudo privs:"
+cat /ccdc/sudoers.txt
+
+
 echo "----------------------"
 echo "SUID binaries:"
-find / -uid 0 -perm -4000 -print 2>/dev/null > /ccdc/suid.txt
 cat /ccdc/suid.txt
